86f06699587102106198f85fa588ee702826997a
Nikita Karak <karak.nikita@gmail.com>
2019-11-18 12:52:31 +0300

    Добавлено создание окружностей случайного цвета. Форма подключается в виде класса.

---
+++ UI.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.create_circle_btn = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.create_circle_btn.sizePolicy().hasHeightForWidth())
+        self.create_circle_btn.setSizePolicy(sizePolicy)
+        self.create_circle_btn.setObjectName("create_circle_btn")
+        self.gridLayout.addWidget(self.create_circle_btn, 0, 0, 1, 1)
+        self.screen_gv = QtWidgets.QGraphicsView(self.centralwidget)
+        self.screen_gv.setObjectName("screen_gv")
+        self.gridLayout.addWidget(self.screen_gv, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и жёлтые окружности"))
+        self.create_circle_btn.setText(_translate("MainWindow", "Создать окружность"))
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import random
+from random import randint
 from PyQt5 import uic, Qt, QtCore, QtGui
 from PyQt5.Qt import Qt, QPointF
-from PyQt5.QtGui import QPen, QBrush
+from PyQt5.QtGui import QPen, QBrush, QColor
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QMainWindow, QGraphicsScene)
+from ui import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.yellow_pen = QPen(Qt.yellow)
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+        self.pen = QPen()
 
         scene = QGraphicsScene()
         scene.setBackgroundBrush(QBrush(Qt.darkGray))
@@ -19,8 +21,9 @@ class MyWidget(QMainWindow):
         self.create_circle_btn.clicked.connect(self.create_circle_handler)
 
     def create_circle_handler(self):
-        d = random.randint(50, min(self.screen_gv.width(), self.screen_gv.height()))
-        item = self.screen_gv.scene().addEllipse(0, 0, d, d, self.yellow_pen)
+        d = randint(50, min(self.screen_gv.width(), self.screen_gv.height()))
+        self.pen.setColor(QColor(randint(0, 255), randint(0, 255), randint(0, 255), 255))
+        item = self.screen_gv.scene().addEllipse(0, 0, d, d, self.pen)
         item.setPos(QPointF(-d / 2, -d / 2))
 
 
